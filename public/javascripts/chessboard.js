4842f186274c424b37b35bc41dbddf64fa88f8ec
Brandon Sprague <brandon00sprague@gmail.com>
2013-10-18 17:27:51 -0400

    Cleaning up the board a little bit

--- public/javascripts/chessboard.js
+++ public/javascripts/chessboard.js
@@ -1,12 +1,15 @@
 Raphael.fn.chessSquare = function(x,y,cell_size){
   var cell = this.rect(x*cell_size,y*cell_size,cell_size,cell_size,5);
   if((x+y) % 2 == 0){
-    cell.attr({fill: 'white'});
+    cell.attr({fill: 'white', 'stroke-width': 0}).data('color','white');
   }else{
-    cell.attr({fill: 'black'});
+    cell.attr({fill: 'black', 'stroke-width': 0}).data('color','black');
   }
   cell.mouseover(function(){
-    this.animate({fill: 'grey'})
+    this.animate({fill: 'grey'},250);
+  })
+  .mouseout(function(){
+    this.animate({fill: cell.data('color')},250);
   });
 }
 
@@ -15,10 +18,10 @@ $.fn.chessboard = function(){
   var cell_size = width/8;
   this.height(width);         //Make the board square
   var paper = Raphael(this.get(0),width,width);
-  paper.rect(0,0,width,width,5);
   for(var x = 0; x < 8; x++){
     for(var y = 0; y < 8; y++){
       var cell = paper.chessSquare(x,y,cell_size);
     }
   }
+  paper.rect(0,0,width,width,5).attr({'stroke-width': 2, stroke: '#000'});
 }
