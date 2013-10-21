8183337dd2fb097a6ce9ada4a8767e1c3353ff52
Brandon Sprague <brandon00sprague@gmail.com>
2013-10-21 15:17:56 -0400

    Restructured code to be object oriented/better

--- public/javascripts/board.js
+++ public/javascripts/board.js
@@ -1,3 +1,3 @@
 $(function(){
-  $('#board').chessboard();
+  var chess = new Chess('board');
 })
--- public/javascripts/chessboard.js
+++ public/javascripts/chessboard.js
@@ -1,27 +1,47 @@
 Raphael.fn.chessSquare = function(x,y,cell_size){
-  var cell = this.rect(x*cell_size,y*cell_size,cell_size,cell_size,5);
+  var cell = this.rect(x*cell_size,y*cell_size,cell_size,cell_size,3);
   if((x+y) % 2 == 0){
     cell.attr({fill: 'white', 'stroke-width': 0}).data('color','white');
   }else{
     cell.attr({fill: 'black', 'stroke-width': 0}).data('color','black');
   }
+
+  //Labeling each cell with algebraic chess notation
+  cell.data('column',String.fromCharCode(x+97));
+  cell.data('row',8-y);
+
   cell.mouseover(function(){
-    this.animate({fill: 'grey'},250);
+    this.animate({fill: '#888'},250);
   })
   .mouseout(function(){
-    this.animate({fill: cell.data('color')},250);
+    this.animate({fill: this.data('color')},250);
+  })
+  .click(function(){
+    var column = this.data('column');
+    var row = this.data('row');
   });
+
+  //Cell Methods
+  cell.boardPos = function(){
+    return cell.data('column')+cell.data('row');
+  }
+
+  return cell
 }
 
-$.fn.chessboard = function(){
+$.fn.chessboard = function(start_fen){
   var width = this.width();
   var cell_size = width/8;
   this.height(width);         //Make the board square
   var paper = Raphael(this.get(0),width,width);
+  var cells = new Array(8);
+  for(var i = 0; i < 8; i++){
+    cells[i] = new Array(8);
+  }
   for(var x = 0; x < 8; x++){
     for(var y = 0; y < 8; y++){
-      var cell = paper.chessSquare(x,y,cell_size);
+      cells[x][y] = paper.chessSquare(x,y,cell_size);
     }
   }
-  paper.rect(0,0,width,width,5).attr({'stroke-width': 2, stroke: '#000'});
+  paper.rect(0,0,width,width,3).attr({'stroke-width': 2, stroke: '#000'});
 }
