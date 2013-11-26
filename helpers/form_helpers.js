dd4744677f0b98ee7eae3f95210e650a5291bd05
rdsilver <rdsilver@student.umass.edu>
2013-11-26 02:12:43 -0500

    Forgot form helper please forgive me oh merciful god

---
+++ helpers/form_helpers.js
@@ -0,0 +1,27 @@
+var contains_bad_word = function(given_string) //Returns true if contains bad words , false otherwise
+{
+	var bad_words = ["penis","vagina","fuck","nigger","douche","faggot","shit","dick","pussy","asshole","crap","poop"];
+
+	for(var x=0;x<bad_words.length;x++)
+    {
+      if(given_string.toLowerCase().indexOf(bad_words[x])>=0)
+       {
+        return true;
+       }
+    }
+
+    return false;
+};
+
+
+var length_between = function(given_string,min,max) //Returns true if between (inclusive) two ints, false otherwise
+{
+  if(given_string.length >=min && given_string.length <=max)
+    return true;
+
+  return false;
+};
+
+
+module.exports.contains_bad_word = contains_bad_word;
+module.exports.length_between = length_between;
