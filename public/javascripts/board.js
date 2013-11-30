95d0070dd22e55262fb539d477993e15c30e07a2
Brandon Sprague <brandon00sprague@gmail.com>
2013-11-30 04:57:40 -0500

    Some more stuff

--- public/javascripts/board.js
+++ public/javascripts/board.js
@@ -21,20 +21,21 @@ $(function(){
     var name = data.name.toLowerCase();
     var table = $('#game_list').find('table');
     var rows = table.find('tr:not(:first)');
+    var new_row = $(data.row);
     if(rows.length == 0 || rows.first().find('td:first').text().toLowerCase() > name){
-      table.find('tr:first').after(data.row);
+      table.find('tr:first').after(new_row);
     }else{
       var added = false
       rows.each(function(){
         var game_name = $(this).find('td:first').text().toLowerCase();
         if(game_name > name){
-          $(this).before(data.row);
+          $(this).before(new_row);
           added = true;
           return false;
         }
       });
       if(!added){
-        table.append(data.row);
+        table.append(new_row);
       }
     }
   });
