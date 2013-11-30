f85fc2aa6707e7f330b313f524ec8497684b0bcb
Brandon Sprague <brandon00sprague@gmail.com>
2013-11-30 04:25:55 -0500

    Sorting new entries

--- public/javascripts/board.js
+++ public/javascripts/board.js
@@ -18,9 +18,25 @@ $(function(){
 
   socket.on('create',function(data){
     //Put the new row in the correct location
-    var name = data.name;
-    var rows = $('#game_list').find('table tr:not(:first)');
-    $('#game_list').find('table').append(data.row);
+    var name = data.name.toLowerCase();
+    var table = $('#game_list').find('table');
+    var rows = table.find('tr:not(:first)');
+    if(rows.length == 0 || rows.first().find('td:first').text().toLowerCase() > name){
+      table.find('tr:first').after(data.row);
+    }else{
+      var added = false
+      rows.each(function(){
+        var game_name = $(this).find('td:first').text().toLowerCase();
+        if(game_name > name){
+          $(this).before(data.row);
+          added = true;
+          return false;
+        }
+      });
+      if(!added){
+        table.append(data.row);
+      }
+    }
   });
 
 });
